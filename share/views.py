0d4c4b843c912d28b61dc8611c6179a56f132224
samarth <samarth@ubuntu.ubuntu-domain>
2014-04-07 15:21:24 +0530

    upload added

--- share/views.py
+++ share/views.py
@@ -6,13 +6,13 @@ from django.template import RequestContext
 import dropbox
 from share.models import FileData
 
-app_key = '4z2auu2o4v1ms76'
+"""app_key = '4z2auu2o4v1ms76'
 app_secret = '8c29694u73eb515'
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 authorize_url = flow.start()
 code = 'P870X6WAn0cAAAAAAAASdK9LmT_Vrd_md9OhhcPwhYI'
-access_token, user_id = flow.finish(code)
-client = dropbox.client.DropboxClient(access_token)
+access_token, user_id = flow.finish(code)"""
+client = dropbox.client.DropboxClient('P870X6WAn0cAAAAAAAASc3c-hsgqIOsdo5crALuuL5e6dyMffAyCznBJjvz9Cp1r')
 #print client.account_info()
 #response = client.put_file('/magnum-opus.txt', f)
 
@@ -33,11 +33,16 @@ def home(request):
 
 def file_submit(request):
 	if request.method == "POST":
-		return HttpResponse(client.account_info())
+		#return HttpResponse(request.POST)
 		course_name = request.POST['course-name']
 		department_code = request.POST['dept']
 		course_code = request.POST['course-code']
 		category = request.POST['category']
+		f = request.FILES['file']
+		name = '/'+f.name 
+		response = client.put_file(name, f)
+		return HttpResponse(response)
+
 		a = FileData(course_name = course_name, department_code = department_code, course_code = course_code, category = category)
 
 		if request.POST['year']!='':
